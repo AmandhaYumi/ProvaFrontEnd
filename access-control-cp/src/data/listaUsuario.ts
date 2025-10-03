ef7f29b9ba63a4175b3250b780532899f5d44779
AmandhaYumi <mandhayumi@gmail.com>
2025-10-03 09:05:11 -0300

    Adicionando type e lista de usuario e página de error e home

---
+++ access-control-cp/src/data/listaUsuario.ts
@@ -0,0 +1,9 @@
+import type { TipoUsuario } from "../types/tipoUsuario";
+
+export const listaUsuarios: TipoUsuario[] = [
+  { id: 1, nome: "Amandha Yumi", nomeUsuario: "yumizxs", email: "mandhayumi@gmail.com" },
+  { id: 2, nome: "Erick Takeshi", nomeUsuario: "EtvTaeyo", email: "ericktakeshi@gmail.com" },
+  { id: 3, nome: "Nana Bardella", nomeUsuario: "nanabardella", email: "nanabardella@gmail.com" },
+  { id: 4, nome: "Juliana Stigliani", nomeUsuario: "justigliani", email: "justigliani@gmail.com" },
+  { id: 5, nome: "Alexandre Carlos", nomeUsuario: "AleCarlosdeJesus", email: "alecarlos@gmail.com" }
+];

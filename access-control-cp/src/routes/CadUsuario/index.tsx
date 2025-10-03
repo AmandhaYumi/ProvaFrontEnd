8636396eeefc58d59349601bd2417b20b20e9d1b
justigliani <julianasilva.vania@gmail.com>
2025-10-03 16:48:36 -0300

    adicionando verificação de duplicidade no cadastro de usuários

--- access-control-cp/src/routes/CadUsuario/index.tsx
+++ access-control-cp/src/routes/CadUsuario/index.tsx
@@ -3,21 +3,37 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import type { TipoUsuario } from "../../types/tipoUsuario";
- 
+
 const URL_API = import.meta.env.VITE_URL_API_USUARIOS;
- 
+
 export default function CadUsuario() {
- 
+
   useEffect(() => {
     document.title = "Cadastros de Usuário";
   }, []);
- 
+
   const navigate = useNavigate();
- 
+
   const { handleSubmit, register, formState: { errors, isSubmitting, isValid } } = useForm<TipoUsuario>({ mode: "onChange" });
- 
+
   const onSubmit = async (dados: TipoUsuario) => {
     try {
+      const responseUsuario = await fetch(`${URL_API}?nomeUsuario=${dados.nomeUsuario}`);
+      const usuariosComMesmoNome = await responseUsuario.json();
+
+      if (usuariosComMesmoNome.length > 0) {
+        alert("Já tem!");
+        return;
+      }
+
+      const responseEmail = await fetch(`${URL_API}?email=${dados.email}`);
+      const usuariosComMesmoEmail = await responseEmail.json();
+
+      if (usuariosComMesmoEmail.length > 0) {
+        alert("Opa! Esse email já tem cadastro aqui");
+        return;
+      }
+
       await fetch(URL_API, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -25,7 +41,7 @@ export default function CadUsuario() {
       });
       alert("Usuário cadastrado!");
       navigate("/login");
- 
+
     } catch (erro) {
       console.error("Erro no cadastro", erro);
       alert("Ocorreu um erro, tente novamente");
@@ -68,7 +84,7 @@ export default function CadUsuario() {
           />
           {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
         </div>
- 
+
         <div className="space-y-1">
           <label htmlFor="senha" className="block text-sm font-medium">Senha:</label>
           <input
@@ -80,7 +96,7 @@ export default function CadUsuario() {
           />
           {errors.senha && <p className="text-red-500 text-sm">{errors.senha.message}</p>}
         </div>
- 
+
         <button type="submit" disabled={isSubmitting || !isValid} className="botaoenviar">
           {isSubmitting ? "Cadastrando..." : "Cadastrar"}
         </button>

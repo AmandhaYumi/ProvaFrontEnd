b7ab55b315c9288dde62867c7872bc407f256b6e
Etvtaeyo <ericknakajune10@gmail.com>
2025-10-04 01:58:37 -0300

    colocando animação

--- access-control-cp/src/routes/Logado/index.tsx
+++ access-control-cp/src/routes/Logado/index.tsx
@@ -6,7 +6,7 @@ export default function Logado() {
     <main>
       <h1>Falando com o SugarT :)</h1>
 
-      <img src={SugartImg} alt="SugarT cupcake cantor" style={{ width: "300px", margin: "20px 0" }} />
+      <img src={SugartImg} alt="SugarT cupcake cantor" className="w-[300px] mx-auto my-5 rounded-2xl animate-bounce" style={{ width: "300px", margin: "20px 0" }} />
       <p>Olá! Eu sou o SugarT! Sou um cantor e tenho esse nome porque vim do reino dos doces para espalhar minha doçura enquanto canto por aí. Ouvi dizer que depois de uma certa Sprint vocês andam precisando, não é?
         Sabia que eu fui inventado na Inglaterra? SugarT… Sugar Tea… HAHAHAHA! Falando em Inglaterra, eu já cantei junto com o Pink Floyd, os Beatles, Queen, Rolling Stones, Black Sabbath e muitos outros…
         Rumores dizem que eu quem os ensinei… mas não quero me gabar!

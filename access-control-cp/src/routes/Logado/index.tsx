5f8552cdd5099cf4e45cd0a8a50aac89a17a1a68
nanabardella <nanabardella@gmail.com>
2025-10-04 16:59:09 -0300

    Adicionando um easteregg na página Logado.

--- access-control-cp/src/routes/Logado/index.tsx
+++ access-control-cp/src/routes/Logado/index.tsx
@@ -1,71 +1,109 @@
-import SugartImg from "../../assets/sugart.jpg"; 
+import { useState } from "react";
+import SugartImg from "../../assets/sugart.jpg";
+import { Link } from "react-router-dom";
+
 export default function Logado() {
   document.title = "Logado";
+
+  const [clickCount, setClickCount] = useState(0);
+  const [mostrarEasterEgg, setMostrarEasterEgg] = useState(false);
+
+  const handleClick = () => {
+    const contador = clickCount + 1;
+    setClickCount(contador);
+
+    if (contador === 10) {
+      setMostrarEasterEgg(true);
+      setClickCount(0); 
+
+      setTimeout(() => setMostrarEasterEgg(false), 6000);
+    }
+  };
 
   return (
     <main>
       <h1>Falando com o SugarT :)</h1>
 
-      <img src={SugartImg} alt="SugarT cupcake cantor" className="w-[300px] mx-auto my-5 rounded-2xl animate-bounce" style={{ width: "300px", margin: "20px 0" }} />
-      <p>Olá! Eu sou o SugarT! Sou um cantor e tenho esse nome porque vim do reino dos doces para espalhar minha doçura enquanto canto por aí. Ouvi dizer que depois de uma certa Sprint vocês andam precisando, não é?
-        Sabia que eu fui inventado na Inglaterra? SugarT… Sugar Tea… HAHAHAHA! Falando em Inglaterra, eu já cantei junto com o Pink Floyd, os Beatles, Queen, Rolling Stones, Black Sabbath e muitos outros…
-        Rumores dizem que eu quem os ensinei… mas não quero me gabar!
-        Espero que eu consiga adoçar seu dia! :)</p>
+      <img
+        src={SugartImg}
+        alt="SugarT cupcake cantor"
+        onClick={handleClick}
+        className="w-[300px] mx-auto my-5 rounded-2xl animate-bounce cursor-pointer active:scale-95 transition-transform"
+        style={{ width: "300px", margin: "20px 0" }}
+      />
 
-        <p> Continuo espalhando minha doçura por aí, tem uma música que combina demais comigo: “Sugar Talking” da Sabrina Carpenter!  
-        Sempre que escuto, sinto minhas notas dançarem e até meus doces preferidos começam a cantar junto! HAHAHA  
-        Espero que essa música adoçe também o seu dia e faça você sorrir tanto quanto eu quando canto por aí!</p>
-        <h2>Sugar Talking- Sabrina Carpenter</h2> 
-        <p>Ooh-ooh, ooh-ooh-ooh
-          Ooh-ooh-ooh-ooh
+      {mostrarEasterEgg && (
+        <div className="bg-white-200 p-5 rounded-2xl shadow-lg max-w-md mx-auto animate-pulse mb-4">
+          <p>
+            Surpresa! Você descobriu o meu segredo!
+            Estou te mandando um doce abraço açucarado!
+          </p>
+        </div>
+      )}
 
-          Put your loving where your mouth is
-          Your sugar talking isn't working tonight, oh
-          Put your loving where your mouth is
-          Yeah, your paragraphs mean shit to me
-          Get your sorry ass to mine
+      <p>
+        Olá! Eu sou o SugarT! Sou um cantor e tenho esse nome porque vim do
+        reino dos doces para espalhar minha doçura enquanto canto por aí. Ouvi
+        dizer que depois de uma certa Sprint vocês andam precisando, não é?
+        Sabia que eu fui inventado na Inglaterra? SugarT… Sugar Tea…
+        HAHAHAHA! Falando em Inglaterra, eu já cantei junto com o Pink Floyd,
+        os Beatles, Queen, Rolling Stones, Black Sabbath e muitos outros…
+        Rumores dizem que eu quem os ensinei… mas não quero me gabar! Espero
+        que eu consiga adoçar seu dia! :)
+      </p>
 
-          Ooh-ooh, ooh-ooh-ooh
-          Ooh-ooh-ooh-ooh
+      <p>
+        Continuo espalhando minha doçura por aí, tem uma música que combina
+        demais comigo: “Sugar Talking” da Sabrina Carpenter! Sempre que escuto,
+        sinto minhas notas dançarem e até meus doces preferidos começam a cantar
+        junto! HAHAHA Espero que essa música adoce também o seu dia e faça você
+        sorrir tanto quanto eu quando canto por aí!
+      </p>
 
-          Saying that you miss me
-          Boy, do you win a prize?
-          You're havin' these epiphanies
-          Big word for a real small mind
-          And aren't you tired of saying a whole lot of nothing?
+      <h2>Sugar Talking - Sabrina Carpenter</h2>
 
-          You tell me that you want me (want me)
-          But, baby, if you need me (need me) (yeah)
-
-          Put your loving where your mouth is
-          Your sugar talking isn't working tonight, oh
-          Say you're a big changed man, I doubt it
-          Yeah, your paragraphs mean shit to me
-          Get your sorry ass to mine
-
-          Ooh-ooh, ooh-ooh-ooh
-          Ooh-ooh-ooh-ooh
-
-          You filled my whole apartment
-          With flowers that died
-          The first to open up your wallet
-          But the last one to flag, a heart only breaks so many times
-          Save your money and stop makin' me cry
-
-          You tell me that you want me (want me) (oh-oh)
-          But, baby, if you need me (need me) (yeah)
-          Oh-oh-oh
-
-          Ooh-ooh, ooh-ooh-ooh
-          Ooh-ooh-ooh-ooh
-
-          Put your loving where your mouth is
-          Your sugar talking isn't working tonight, oh
-          Put your loving where your mouth is
-          Yeah, your paragraphs mean shit to me
-          It's verbatim what you said last week
-          It's your seventh last chance, honey
-          Get your sorry ass to mine</p>
-    </main> 
+      <p>
+        Ooh-ooh, ooh-ooh-ooh
+        Ooh-ooh-ooh-ooh
+        Put your loving where your mouth is
+        Your sugar talking isn't working tonight, oh
+        Put your loving where your mouth is
+        Yeah, your paragraphs mean shit to me
+        Get your sorry ass to mine
+        Ooh-ooh, ooh-ooh-ooh
+        Ooh-ooh-ooh-ooh
+        Saying that you miss me
+        Boy, do you win a prize?
+        You're havin' these epiphanies
+        Big word for a real small mind
+        And aren't you tired of saying a whole lot of nothing?
+        You tell me that you want me (want me)
+        But, baby, if you need me (need me) (yeah)
+        Put your loving where your mouth is
+        Your sugar talking isn't working tonight, oh
+        Say you're a big changed man, I doubt it
+        Yeah, your paragraphs mean shit to me
+        Get your sorry ass to mine
+        Ooh-ooh, ooh-ooh-ooh
+        Ooh-ooh-ooh-ooh
+        You filled my whole apartment
+        With flowers that died
+        The first to open up your wallet
+        But the last one to flag, a heart only breaks so many times
+        Save your money and stop makin' me cry
+        You tell me that you want me (want me) (oh-oh)
+        But, baby, if you need me (need me) (yeah)
+        Oh-oh-oh
+        Ooh-ooh, ooh-ooh-ooh
+        Ooh-ooh-ooh-ooh
+        Put your loving where your mouth is
+        Your sugar talking isn't working tonight, oh
+        Put your loving where your mouth is
+        Yeah, your paragraphs mean shit to me
+        It's verbatim what you said last week
+        It's your seventh last chance, honey
+        Get your sorry ass to mine
+      </p>
+    </main>
   );
 }
